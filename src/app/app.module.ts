a991b64398a8ce7b6a10534c2d581e7d0e94061f
pravalikarao <pravalikarao55@gmail.com>
2021-06-07 21:16:51 +0530

    routing and navigation is implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     FooterComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    RegisterComponent,
+    HomeComponent,
+    ContactusComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
