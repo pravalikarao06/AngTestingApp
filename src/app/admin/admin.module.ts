95652760f3212839c192512c33e673965adcc6bd
pravalikarao <pravalikarao55@gmail.com>
2021-06-13 13:20:37 +0530

    created viewproducts comp and displayed data in table

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { ViewproductsComponent } from './viewproducts/viewproducts.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    ViewproductsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/admin/viewproducts/viewproducts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { Mobile } from 'src/app/models/mobile.model';
+
+@Component({
+  selector: 'app-viewproducts',
+  templateUrl: './viewproducts.component.html',
+  styleUrls: ['./viewproducts.component.css']
+})
+export class ViewproductsComponent implements OnInit {
+
+
+  mobiles:Mobile[]=[];
+  constructor(private dsObj:DataService) { }
+
+  ngOnInit(): void {
+    this.dsObj.getMobilesData().subscribe(
+      res=>{
+        this.mobiles=res;
+      },
+      err=>{
+        console.log("err in displaying viewproducts is",err)
+      }
+    )
+  }
+
+}
